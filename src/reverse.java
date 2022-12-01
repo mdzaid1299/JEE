c218db81353266cc07e8e7005b5db0f5ee3d4b53
HP <mdzaid1299@gmail.com>
2022-12-01 11:07:39 +0530

    ini

--- src/reverse.java
+++ src/reverse.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class reverse {
+public class reverse {
+
+        public static void main(String[] args)
+        {
+            String str="Happy";
+            String reverse="";
+            for(int i=str.length()-1;i>=0;i--)
+            {
+                reverse=reverse+str.charAt(i);
+            }
+            System.out.println("Reverse of Happy is:"+reverse);
+        }
 }
