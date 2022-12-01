705954f62421db456abb5aef162e3930927f1c52
HP <mdzaid1299@gmail.com>
2022-12-01 23:31:01 +0530

    ini

---
+++ src/Static.java
@@ -0,0 +1,11 @@
+public class Static {
+    //static Block
+    static{
+        System.out.println("Hum First");
+
+    }
+//static block is executed before Main method
+    public static void main(String[] args) {
+        System.out.println("Arra hu bhai");
+    }
+}
